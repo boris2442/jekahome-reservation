b4cade6c733585148e1a070cfbb13bc989342609
boris2442 <aubinborissimotsebo@gmail.com>
2025-05-12 11:06:39 +0200

    page not found

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Page non trouvée - JekaHome</title>
+ <link rel="stylesheet" href="./src/output.css">
+</head>
+<body class="bg-[#F3F4F6] min-h-screen flex items-center justify-center px-6">
+
+  <div class="text-center max-w-md">
+    <h1 class="text-[10rem] font-extrabold text-[#1E3A8A] leading-none">404</h1>
+    <h2 class="text-2xl font-semibold text-[#1E3A8A] mt-2">Page introuvable</h2>
+    <p class="mt-4 text-[#555]">Désolé, la page que vous recherchez n’existe pas ou a été déplacée.</p>
+
+    <a href="/" class="mt-6 inline-block bg-[#A8BDB3] text-white font-medium px-6 py-3 rounded-lg shadow hover:bg-[#D9B08C] transition duration-300">
+      Retour à l'accueil
+    </a>
+  </div>
+
+</body>
+</html>
